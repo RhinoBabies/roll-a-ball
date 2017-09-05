95ae197330bb237ec3544b5fad2781845fb33b58
Justin <vessdawg@gmail.com>
2017-09-04 21:46:04 -0700

    First commit. Game walls complete; need collectables.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	private Rigidbody rb; //where the GameObject will store its Rigidbody component's id
+	public float speed;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody> (); //grabs reference to this GameObject's Rigidbody component
+	}
+
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * speed); //when pushing correct keys, adds physics forces to the Rigidbody component for movement
+	}
+}
